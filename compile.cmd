83aaca5351115062840f2d38b8a3e291e4e5481b
Dirk Arnez <smalldirkalex@gmail.com>
2022-08-18 17:54:47 +0800

    - init, working

---
+++ compile.cmd
@@ -0,0 +1,4 @@
+@echo off
+set PATH=D:\Softwares\autoit-v3;D:\Softwares\autoit-v3\Aut2Exe
+
+start Aut2exe_x64.exe /in calc.au3 /out calc.exe
---
+++ run.cmd
@@ -0,0 +1,4 @@
+@echo off
+set PATH=D:\Softwares\autoit-v3
+
+start AutoIt3_x64.exe calc.au3
